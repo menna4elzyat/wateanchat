18c5088c1e05a2dd13293283e0c26b3ed3866601
menna elzyat <153638831+menna4elzyat@users.noreply.github.com>
2025-11-27 20:11:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -183,5 +183,7 @@ async def exception_handler(request: Request, exc: Exception):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    import os
+    port = int(os.environ.get("PORT", 8000))  # Railway بيحدد الـ port تلقائي
+    uvicorn.run("main:app", host="0.0.0.0", port=port, log_level="info")
 
